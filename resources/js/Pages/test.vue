d0edf2b6503af4dc2e9eba195627c6ff74dd3971
Venz Olarte <venzolarte@gmail.com>
2025-07-21 00:05:14 +0800

    add laravel

---
+++ resources/js/Pages/test.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+defineProps({
+    name: String,
+})
+
+</script>
+
+<template>
+    <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content text-center">
+            <div class="max-w-md">
+                <h5 class="text-2xl font-bold animate-bounce">Made with ❤ by {{ name }}!</h5>
+            </div>
+        </div>
+    </div>
+</template>
+
